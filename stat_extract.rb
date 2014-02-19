571c0c7ba33a1e845efda3a811d8c56fb147e57b
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2014-02-19 10:15:49 -0500

    Refined the hashes that will be used for statistic calculations

--- stat_extract.rb
+++ stat_extract.rb
@@ -2,4 +2,18 @@
 
 require 'csv'
 
-row_hashes = CSV.parse(File.read('data/tool_list_research.csv'), headers: true).each_entry.map(&:to_h)
+parsed_csv = CSV.parse(File.read('data/tool_list_research.csv'), headers: true)
+data = parsed_csv.map do |row|
+  data = {}
+
+  row.to_h.each do |key, value|
+    next if key.nil? || value.nil?
+    new_key = key.downcase.gsub(" ", "_").to_sym
+    data[new_key] = value
+  end
+
+  data
+end
+
+
+puts data.inspect
